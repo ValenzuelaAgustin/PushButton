a6ab8ecb046c4ecab452e73f9cf921e5dc7f15ae
Agustin Valenzuela <valenzuela.agustin.tomas@gmail.com>
2023-08-10 20:51:48 -0300

    .

--- example/example.ino
+++ example/example.ino
@@ -5,13 +5,17 @@ https://wokwi.com/projects/372717884958772225
 */
 
 PushButton p1 (2, INPUT_PULLUP);
+#define p2 19
 
 int last = 0;
 int counter = 0;
+int last2 = 0;
+int counter2 = 0;
 
 void setup()
 {
-    Serial.begin(115200);
+    Serial.begin(9600);
+    pinMode(p2, INPUT_PULLUP);
 }
 
 void loop()
@@ -22,5 +26,12 @@ void loop()
         Serial.print("Counter: ");
         Serial.println(counter);
         last = counter;
+    }
+    if (!digitalRead(p2)) ++counter2;
+    if (last2 != counter2)
+    {
+        Serial.print("\t\tCounter2: ");
+        Serial.println(counter2);
+        last2 = counter2;
     }
 }

3a42d75f1ea59d963031a5ba24912c18e1fc2543
Agustin Valenzuela <valenzuela.agustin.tomas@gmail.com>
2023-08-11 15:04:13 -0300

    example update.

--- example/example.ino
+++ example/example.ino
@@ -7,9 +7,7 @@ https://wokwi.com/projects/372717884958772225
 PushButton p1 (2, INPUT_PULLUP);
 #define p2 19
 
-int last = 0;
 int counter = 0;
-int last2 = 0;
 int counter2 = 0;
 
 void setup()
@@ -20,18 +18,16 @@ void setup()
 
 void loop()
 {
-    if (!p1.Read()) ++counter;
-    if (last != counter)
+    if (!p1.Read())
     {
         Serial.print("Counter: ");
         Serial.println(counter);
-        last = counter;
+        ++counter;
     }
-    if (!digitalRead(p2)) ++counter2;
-    if (last2 != counter2)
+    if (!digitalRead(p2))
     {
         Serial.print("\t\tCounter2: ");
         Serial.println(counter2);
-        last2 = counter2;
+        ++counter2;
     }
 }
